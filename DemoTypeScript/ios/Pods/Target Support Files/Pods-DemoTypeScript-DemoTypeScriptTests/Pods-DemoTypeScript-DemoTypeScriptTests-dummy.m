5fcd2a20fe25909e383cb6c82b83d653ac0e28a7
thuantv11 <vinhthuan.tr@gmail.com>
2021-07-03 17:31:58 +0700

    #push ios folder

---
+++ DemoTypeScript/ios/Pods/Target Support Files/Pods-DemoTypeScript-DemoTypeScriptTests/Pods-DemoTypeScript-DemoTypeScriptTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoTypeScript_DemoTypeScriptTests : NSObject
+@end
+@implementation PodsDummy_Pods_DemoTypeScript_DemoTypeScriptTests
+@end
---
+++ DemoTypeScript/ios/Pods/Target Support Files/Pods-DemoTypeScript/Pods-DemoTypeScript-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoTypeScript : NSObject
+@end
+@implementation PodsDummy_Pods_DemoTypeScript
+@end
